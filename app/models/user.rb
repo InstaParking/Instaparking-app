c11d5048141ac251d97269351cbd452b4f2d8563
Adaia <adaia.onice@gmail.com>
2016-05-24 02:51:26 +0000

    agregar sobre nosotros

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
          
   has_one :profile
+  has_many :organization
+  has
   
   enum role: [:user, :operator, :chef, :admin]
 
