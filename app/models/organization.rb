9833f50bd649a8c10e152ce54ac2754b9087fb8f
Adaia <adaia.onice@gmail.com>
2016-05-31 02:57:18 +0000

    organizacion y miembros de la organizacion

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,14 @@
 class Organization < ActiveRecord::Base
-  belongs_to :user
+    belongs_to :user
+   # belongs_to :profile
+    has_many :organization_member 
+    
+    validates :name, presence: true
+    validates :ruc, presence: true
+    validates :address, presence: true
+    validates :user, presence: true
+ 
+ 
 end
+
+
